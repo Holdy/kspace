fc356ed17430c66784b62df0b4d4094636bc60c4
Chris Holden <christopherholden@hotmail.com>
2021-02-08 09:05:51 +0000

    Added country2 and country3 to for ISO 3166 country codes

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+    bytes_downloaded_per_second: 'bytes_downloaded_per_second',
+    bytes_uploaded_per_second:'bytes_uploaded_per_second',
+
+    mac_address:'mac_address',
+    device_name:'device_name',
+
+    power_state:'power_state',
+
+    connected_devices:'connected_devices',
+    latitude:'latitude',
+    longitude:'longitude',
+
+    iteration_progress_current:'iteration_progress_current',
+    iteration_progress_maximum:'iteration_progress_maximum',
+    iteration_unit_name:'iteration_unit_name',
+    iteration_task_name:'iteration_task_name',
+    iteration_task_agent:'iteration_task_agent',
+
+    address_lines:'address_lines',
+    postal_code:'postal_code',
+    
+    pal_id: 'pal_id',
+    store_id:'store_id',
+
+    country2: 'country2',
+    country3: 'country3'
+    
+};
